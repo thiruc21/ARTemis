6b982f81fd91eec3a3cfeae10a7666198c24622d
VishakanShan <vishakan.shanthakumar@mail.utoronto.ca>
2018-03-17 19:43:25 -0400

    Implemented WebRTC chat

--- Drawathon/src/app/app-routing.module.ts
+++ Drawathon/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import  { GameComponent } from './components/game/game.component';
 import { MainComponent } from './components/main/main.component';
 import { LobbyComponent } from './components/lobby/lobby.component';
-
+import { ChatComponent } from './components/chat/chat.component';
 const routes: Routes = [
   { path: 'game', component:GameComponent } ,
   { path: '', component:MainComponent, pathMatch: 'full' } ,
-  { path: 'lobby', component:LobbyComponent }
+  { path: 'lobby', component:LobbyComponent },
+  { path: 'test', component:ChatComponent }
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- Drawathon/src/app/components/chat/chat.component.ts
+++ Drawathon/src/app/components/chat/chat.component.ts
@@ -7,13 +7,48 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
   messages:string[];
+  myPeerId:string;
+  peer;  
   constructor() { }
   @ViewChild('textarea') public textarea:ElementRef;
+  @ViewChild('peer') public peerId:ElementRef;
   ngOnInit() {
     this.messages = [];
+    this.myPeerId = "";
+    this.peer = new Peer({key: 'lwjd5qra8257b9'}); //Register our own...!!!
+    this.peer.on('open', function(id){
+      console.log(id);
+    });
+    setTimeout(() => {
+      this.myPeerId = this.peer.id;
+    },3000);
+    var messages:string[] = this.messages;
+    this.peer.on('connection', function(connection) {
+      connection.on('data', function(data){
+        messages.push(data);
+      });
+    });
+    this.timeOut();
   }
   submitMessage() {
-    this.messages.push("Null User: " + this.textarea.nativeElement.value);
+    var text = this.textarea.nativeElement.value;
+    var me = this.myPeerId;
+    this.messages.push(me + ": " + text);
     this.textarea.nativeElement.value = "";
+    var conn = this.peer.connect(this.peerId.nativeElement.value);
+    conn.on('open', function(){
+      conn.send(me + ": " + text);
+    });
   }
+  timeOut(){
+    setTimeout(() => {
+      this.update();
+      this.timeOut();
+    }, 1000);
+  }
+  update(){
+    var message = this.messages.pop();
+    this.messages.push(message);
+  }
+  
 }
