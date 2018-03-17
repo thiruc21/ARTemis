4a1b4a8172a38e14055fa3ba7252514499612a9c
VishakanShan <vishakan.shanthakumar@mail.utoronto.ca>
2018-03-17 18:07:32 -0400

    Finished lobby

--- Drawathon/src/app/components/lobby/lobby.component.ts
+++ Drawathon/src/app/components/lobby/lobby.component.ts
@@ -8,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class LobbyComponent implements OnInit {
   team1:string[];
   team2:string[];
+  host:string;
   constructor() { }
 
   ngOnInit() {
-    
+    this.host = "Null Host"
+    this.team1 = ["Null User", "Null User"];
+    this.team2 = ["Null User", "Null User"];
   }
 
 }
