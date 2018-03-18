b38673b1212af773b1964345b7e215ccb0e8197b
Vijanthan Thiruchelvarajah <thiruc21@IITS-B473-28.utsc-labs.utoronto.ca>
2018-03-18 14:24:32 -0400

    Fixed PeerJS issues and allowed peers to stay alive without disconnecting

--- Drawathon/src/app/components/chat/chat.component.ts
+++ Drawathon/src/app/components/chat/chat.component.ts
@@ -27,29 +27,46 @@ export class ChatComponent implements OnInit {
       this.myPeerId = this.peer.id;
     },3000);
     var messages:string[] = this.messages;
+    // Receive the data
     this.peer.on('connection', function(connection) {
       connection.on('data', function(data){
         messages.push(data);
       });
     });
     this.timeOut();
+    this.keepAlive();
   }
+  // Push the message, connect to the peer, and send message to the peer
   submitMessage() {
     var text = this.textarea.nativeElement.value;
     var me = this.myPeerId;
     this.messages.push(me + ": " + text);
     this.textarea.nativeElement.value = "";
-    var conn = this.peer.connect(this.peerId.nativeElement.value);
-    conn.on('open', function(){
-      conn.send(me + ": " + text);
+    // Connect to other peer and send message
+    var otherPeer = this.peer.connect(this.peerId.nativeElement.value);
+    otherPeer.on('open', function(){
+      otherPeer.send(me + ": " + text);
     });
   }
+
   timeOut(){
+    // Update messages every second
     setTimeout(() => {
       this.update();
       this.timeOut();
     }, 1000);
   }
+
+  
+  keepAlive(){
+    // Keep the peer alive as long as on page
+    setTimeout(() => {
+       // Connect to other peer and send message
+       var conn = this.peer.connect(this.peerId.nativeElement.value);
+       this.keepAlive();
+    }, 25000);
+  }
+  // Update 
   update(){
     var message = this.messages.pop();
     this.messages.push(message);
