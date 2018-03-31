805278b15907a24bcb3f980687815fc42462419f
shadidsh <shadidsh@IITS-I406-09.utsc-labs.utoronto.ca>
2018-03-31 15:47:55 -0400

    pass username messages

--- Drawathon/app.js
+++ Drawathon/app.js
@@ -181,15 +181,15 @@ var isAuthenticated = function(req, res, next) {
 var checkUsername = function(req, res, next) {
     req.checkBody('username', 'Username is required').exists().notEmpty();
     req.checkBody('username', 'Please enter a valid alphanumeric username').isAlphanumeric();
-    req.checkBody('username', 'Username must be atleast 5 characters and a max of 24 characters').isLength({min:5})
-    req.checkBody('username', 'Username must be atleast 5 characters and a max of 24 characters').isLength({max: 24})
+    req.checkBody('username', 'Username must be atleast 5 characters ').isLength({min:5});
+    req.checkBody('username', 'Username must be atmost 24 characters long').isLength({max: 24});
     next();
 };
 
 var checkPassword = function(req, res, next) {
     req.checkBody('password', 'Password is required').exists().notEmpty();
-    req.checkBody('password', 'Password must be atleast 5 characters long').isLength({min:5})
-    req.checkBody('password', 'Password must be a max of 40 characters').isLength({max: 40})
+    req.checkBody('password', 'Password must be atleast 5 characters ').isLength({min:5});
+    req.checkBody('password', 'Password must be atmost 40 characters long').isLength({max: 40});
     next();
 };
 
