4c9693710022f88b49db7d68f773dd1160d42691
VishakanShan <vishakan.shanthakumar@mail.utoronto.ca>
2018-04-01 17:50:02 -0400

    Cleaned canvas code

--- Drawathon/src/app/components/canvas/canvas.component.ts
+++ Drawathon/src/app/components/canvas/canvas.component.ts
@@ -63,11 +63,11 @@ export class CanvasComponent implements OnInit {
     this.recieved = false; // Recieved flag. To recieve from parent.
     this.singlePlayer = true; // Single player flag, to recieve from parent. Default true.
 
-    this.canvasElem = this.canvas.nativeElement;
+    this.canvasElem = this.canvas.nativeElement; // Get canvas context.
     this.ctx = this.canvasElem.getContext('2d');
     
 
-    // Connect to peer.
+    // Generate peerId.
     this.peer = new Peer({host : "lightpeerjs.herokuapp.com",
                           secure : true,
                           path : "/peerjs",
@@ -77,54 +77,61 @@ export class CanvasComponent implements OnInit {
       console.log(id);
     });
     setTimeout(() => {
-      this.myPeerId = this.peer.id;
+      this.myPeerId = this.peer.id; // Update peer info.
     },2000);
 
 
-    // Assign variables that can be used in the callback
-    var peerDraw = this.peerEdit;
+    
+    var peerDraw = this.peerEdit; // Assign reference to list that can be used in scope.
     // Receive the data
     this.peer.on('connection', function(connection) {
-      connection.on('data', function(data){
+      connection.on('data', function(data) {
         while (data.length > 0) {
-          var smtn = data.shift();
+          var smtn = data.shift(); // Temp variable.
           peerDraw.push(smtn);
         }
       });
     });
-    this.timeOut();
-    this.keepAlive();
+    this.timeOut(); // Start peer sharing loop.
+    this.keepAlive(); // Start keep alive for peer connection.
   }
-  test(){
-    console.log(this.cColor);
-  }
-  ngAfterViewInit() {
+
+  ngAfterViewInit() { // Loads after webpage.
     this.ctx.lineJoin = "round";
     this.ctx.lineWidth = 10;
-    this.bound = this.canvasElem.getBoundingClientRect();
+    this.bound = this.canvasElem.getBoundingClientRect(); // Set bound.
   }
-  mouseDown(event){
+
+  mouseDown(event) { // Mouse down, set pressed flag to true, update mouse position.
+    if (this.bg != "white") return; // Disabled canavs, return.
     this.pressed = true;
     this.pts.x = event.clientX - this.bound.left;
     this.pts.y = event.clientY - this.bound.top;
-    if (this.bg == "white" && this.pressed && event.clientX < this.bound.right && event.clientX > this.bound.left && event.clientY < this.bound.bottom && event.clientY > this.bound.top) {
+
+    var withinBounds:boolean = (event.clientX < this.bound.right && event.clientX > this.bound.left && event.clientY < this.bound.bottom && event.clientY > this.bound.top);
+    // Draw a point.
+    if (this.pressed && withinBounds) {
       this.drawPoint();
-  }
+    }
   };
-  mouseUp = function() {
+  mouseUp = function() { // Mouse up, update flag.
     this.pressed = false;
   }
-  mouseMove(event){
+
+  mouseMove(event) { // Update mouse position.
+    if (this.bg != "white") return; // Disabled canavs, return.
     this.pts.px = this.pts.x;
     this.pts.py = this.pts.y;
     this.pts.x = event.clientX - this.bound.left;
     this.pts.y = event.clientY - this.bound.top;
-
-    if (this.bg == "white" && this.pressed && event.clientX < this.bound.right && event.clientX > this.bound.left && event.clientY < this.bound.bottom && event.clientY > this.bound.top) {
+    // Draw a line.
+    var withinBounds:boolean = (event.clientX < this.bound.right && event.clientX > this.bound.left && event.clientY < this.bound.bottom && event.clientY > this.bound.top);
+    if (this.bg == "white" && this.pressed && withinBounds) {
         this.draw();
     }
   }
-  drawPoint() {
+
+  drawPoint() { // Draw a point.
     this.ctx.beginPath();
     this.ctx.fillStyle = this.color;
     var size =  this.size;
@@ -135,7 +142,7 @@ export class CanvasComponent implements OnInit {
     this.ctx.fill();
   }
 
-  draw() {
+  draw() { // Draw a stroke.
     var size = this.size;
     var color = this.color;
     if (this.color == 'custom') color = this.cColor;
@@ -150,40 +157,37 @@ export class CanvasComponent implements OnInit {
     this.ctx.closePath();
     this.ctx.stroke();
 
+    // Push to my edits list.
     this.myEdit.push([this.pts.x, this.pts.y, this.pts.px, this.pts.py, size, this.color])
     // Connect to other peer and send message
   
   }
 
-  clickColor(color) {
+  clickColor(color) { // Update color based of choice.
     this.color = color;
-    console.log(color);
   }
 
-  clickSize(size){
+  clickSize(size) { // Update size based of choice.
     this.size = size;
   }
 
   timeOut(){
     setTimeout(() => {
-      //console.log("canvas recieved?: " + this.recieved);
      if (this.recieved && this.running) this.update();
-     else {
-
-     }
      this.timeOut();
     }, 300);
   }
 
   keepAlive(){
-    // Keep the peer alive as long as on page
     setTimeout(() => { // Keep the peer alive as long as on page
        var conn = this.peer.socket.send({
         type: 'ping'});
        if (this.running) this.keepAlive();
     }, 25000);
   }
-  pDraw(x,y,px,py, size, color) {
+
+  // Draw for recieved data.
+  pDraw(x, y, px, py, size, color) {
     this.ctx.beginPath();
     this.ctx.moveTo(px, py);
     this.ctx.lineTo(x, y);
@@ -192,31 +196,32 @@ export class CanvasComponent implements OnInit {
     this.ctx.closePath();
     this.ctx.stroke();
   }
-   // Update 
-   update(){
-    console.log("Canvas - Single Player?: " + this.singlePlayer);
-    if (this.customSize.nativeElement.value < 5) this.customSize.nativeElement.value = 5
+
+  // Update 
+  update(){
+    if (this.customSize.nativeElement.value < 5) this.customSize.nativeElement.value = 5 // Hard code min and max
     if (this.customSize.nativeElement.value > 60) this.customSize.nativeElement.value = 60
-    this.customSS = this.customSize.nativeElement.value.toString() + 'px'
-    if (this.myEdit.length > 0) {
-      var myEdit = this.myEdit.slice();
-      this.myEdit = [];
-      if (this.singlePlayer == false) {
+    this.customSS = this.customSizeElem.value.toString() + 'px'
+
+    if (this.myEdit.length > 0) { // If there are edits to send.
+      var myEdit = this.myEdit.slice(); // Copy edits.
+      this.myEdit = []; // Empty edit.
+      if (this.singlePlayer == false) { // If multiplayer send to other peer.
         var otherPeer = this.peer.connect(this.myPeers[0]);
         otherPeer.on('open', function(){
           otherPeer.send(myEdit);
         });
-      }
+      } // Always send to host.
       var hostPeer = this.peer.connect(this.myPeers[1]);
       hostPeer.on('open', function(){
         hostPeer.send(myEdit);
       });
     }
+    // Draw what was sent from peer. If single player peerEdit.length = 0;
     while (this.peerEdit.length > 0) {
       var smtn = this.peerEdit.shift();
       this.pDraw(smtn[0], smtn[1], smtn[2],smtn[3],smtn[4],smtn[5]);
     }
-    //  this.draw()
   }
 }
 
